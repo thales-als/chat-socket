28779bd51c177089a44eada3a14327f8e0bacf3d
thales-als <thalessouza.dev@gmail.com>
2024-12-03 14:34:41 -0300

    deus eh mt bom deus eh perfeito

--- src/Client.java
+++ src/Client.java
@@ -12,15 +12,7 @@ public class Client {
     private static boolean isRunning = true;
 
     public static void main(String[] args) {
-        if (args.length > 0) {
-            host = args[0];
-            port = Integer.parseInt(args[1]);
-            System.out.println("Conectando no servidor: " + host);
-            System.out.println("Na porta: " + port);
-        } else {
-            System.out.println("Conectando no servidor padrão: " + host);
-            System.out.println("Na porta padrão: " + port);
-        }
+    	configureServerHostAndPort(args);
 
         try (
             Socket socket = new Socket(host, port);
@@ -28,24 +20,12 @@ public class Client {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Scanner sc = new Scanner(System.in)
         ) {
-            System.out.print("Digite seu nome de usuário: ");
-            String username = sc.nextLine();
-            out.println(username);
-
-            new Thread(() -> {
-                String serverMessage;
-                try {
-                    while (isRunning && (serverMessage = in.readLine()) != null) {
-                        System.out.println(serverMessage);
-                    }
-                } catch (IOException e) {
-                    if (isRunning) {
-                        System.err.println("Erro ao ler mensagens do servidor: " + e.getMessage());
-                    }
-                }
-            }).start();
+        	
+        	getUsername(sc, out);
+        	listenForMessages(in);
 
             String userInput;
+            
             while (isRunning) {
                 userInput = sc.nextLine();
                 if ("/quit".equalsIgnoreCase(userInput)) {
@@ -59,5 +39,38 @@ public class Client {
         } catch (IOException e) {
             System.err.println("Erro ao conectar ao servidor: " + e.getMessage());
         }
+    }
+    
+    public static void configureServerHostAndPort(String[] args) {
+    	if (args.length > 0) {
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+            System.out.println("Conectando no servidor: " + host);
+            System.out.println("Na porta: " + port);
+        } else {
+            System.out.println("Conectando no servidor padrão: " + host);
+            System.out.println("Na porta padrão: " + port);
+        }
+    }
+    
+    public static void getUsername(Scanner sc, PrintWriter out) {
+    	System.out.print("Digite seu nome de usuário: ");
+    	String username = sc.nextLine();
+    	out.println(username);
+    }
+    
+    public static void listenForMessages(BufferedReader in) {
+    	new Thread(() -> {
+            String serverMessage;
+            try {
+                while (isRunning && (serverMessage = in.readLine()) != null) {
+                    System.out.println(serverMessage);
+                }
+            } catch (IOException e) {
+                if (isRunning) {
+                    System.err.println("Erro ao ler mensagens do servidor: " + e.getMessage());
+                }
+            }
+        }).start();
     }
 }
